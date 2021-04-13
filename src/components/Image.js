37e98cfd70c6eb6a8fa8c747ad29fc4e3adb8c03
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-04-13 12:54:56 +0200

    conditional rendering og images in summary

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Image = () => {
+    const quizOver = useSelector((state) => state.quiz.quizOver)
+    const answers = useSelector((state) => state.quiz.answers)
+    let correctAnswers = 0
+
+    for (let answer of answers) {
+        if (answer.isCorrect) {
+            correctAnswers++
+        }
+    }
+
+    const score = (correctAnswers / answers.length)
+
     return (
-        <img src="./assets/nicolas-cage-smiling.png" />
+        <>
+            { !quizOver && <img src="./assets/nicolas-cage-smiling.png" />}
+            { (quizOver && score >= 0.7) && <img src="./assets/nicolas-cage-oscar.png" />}
+            { (quizOver && (score >= 0.5 && score < 0.7)) && <img src="./assets/nicolas-cage-close-up.png" />}
+            { (quizOver && score < 0.5) && <img src="./assets/nicolas-cage-scared.png" />}
+        </>
     )
 }
 
